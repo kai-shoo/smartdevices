9ceab923c4ab5feb2872aaf477e042bfd3463e86
Alexandr Shuldeshov <qsteamk@gmail.com>
2021-06-23 02:58:41 +0300

    добавил планый скролл

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
 "use strict";
-(function () {
-  const header = document.querySelector(`.header`);
-  const buttonToggle = document.querySelector(`.toggle`);
-  const page = document.querySelector(`.page`);
-
-  if (header && buttonToggle && page) {
-    page.classList.remove(`page--nojs`);
-    header.classList.remove(`header--active`);
-
-    const toggleMenu = (event) => {
-      if (
-        (event.target.closest(`.toggle`) && event.type === `click`) ||
-        event.key === `Escape`
-      ) {
-        header.classList.toggle(`header--active`);
-        page.classList.toggle(`page--block`);
-      }
-
-      if (header.classList.contains(`header--active`)) {
-        document.addEventListener(`keydown`, toggleMenu);
-      } else {
-        document.removeEventListener(`keydown`, toggleMenu);
-      }
-    };
-
-    buttonToggle.addEventListener(`click`, toggleMenu);
-    header.addEventListener(`click`, (event) => {
-      if (!event.target.classList.contains(`nav__link`)) {
-        return;
-      }
-      header.classList.remove(`header--active`);
-      page.classList.remove(`page--block`);
-    });
-  }
-})();
-;
 /* eslint-disable new-cap */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
@@ -96,5 +60,20 @@
 
   window.addEventListener(`DOMContentLoaded `, handleMobileWidth(mediaQueryMobile));
   mediaQueryMobile.addEventListener(`change`, handleMobileWidth);
+})();
+;
+(function () {
+  window.addEventListener("hashchange", (e) => e.preventDefault());
+
+  document.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    const anchorLink = e.target.closest(`a[href^='#']`);
+
+    if (anchorLink) {
+      const id = anchorLink.hash;
+      const target = document.querySelector(`${id}`);
+      target.scrollIntoView({ behavior: "smooth", inline: "end" });
+    }
+  });
 })();
 ;
